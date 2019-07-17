31cd2708792c6bf467cc2eb050f116ac2904a23e
Akash Chhabria <akash.chhabria@ryerson.ca>
2019-07-17 17:03:59 -0400

    added new logos and sidebar file

--- src/Component/Options.jsx
+++ src/Component/Options.jsx
@@ -6,16 +6,19 @@ class Options extends Component {
     render() {
         return (
             <div className="Options">
-                <Container>
+                <Container className="Categories-wrap">
                     <Row className="Categories">
                         <Col lg={4}>
-                            <Image src="assets/Photo.png" className="Photography" fluid roundedCircle/>
+                            <a href="#"><Image src="assets/Photo.png"  className="Photography" fluid roundedCircle/></a>
+                            <label className="label">Photography</label>
                         </Col>
                         <Col lg={4}>
-                            <Image src="assets/Prog.png" className="Programming" fluid roundedCircle/>
+                            <a href="#"><Image src="assets/Dev.png" className="Programming" fluid roundedCircle/></a>
+                            <label className="label">Programming</label>
                         </Col>
                         <Col lg={4}>
-                            <Image src="assets/Other.png" className="Other" fluid roundedCircle/>
+                            <a href="#"><Image src="assets/Other.png" className="Other" fluid roundedCircle/></a>
+                            <label className="label" id="project">Project Management</label>
                         </Col>
                     </Row>
 
